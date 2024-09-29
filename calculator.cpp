ab1f1f5b0a89826063ef2e2c46d1bb337b7a173b
OlehSkmrh <oleg.skomoroha1@gmail.com>
2024-09-29 13:32:30 +0300

    add a division operation

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,11 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+int Calculator::Div (double a, double b)
+{
+    if (b == 0) {
+        std::cerr << "Error: Division by zero!" << std::endl;
+        return 0;
+    }
+    return a / b;
+}
